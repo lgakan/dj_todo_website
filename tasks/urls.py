b64bbba135db02bbfb7dabd8970beec9077e6468
lgakan <lgakan@student.agh.edu.pl>
2022-08-08 14:29:07 +0200

    register and login

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('register/', views.register, name="register"),
+    path('login/', views.login_page, name='login'),
+    path('logout/', views.logout_page, name='logout'),
+    # path('login/', views.CustomLoginView.as_view(), name='login'),
+    # path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+
     path('home/', views.home, name='home'),
     path('details/', views.details, name='details'),
 
--- tasks/views.py
+++ tasks/views.py
@@ -1,23 +1,65 @@
 from django.shortcuts import render, redirect
 from .models import Task
-from .forms import TaskForm
+from .forms import TaskForm, CreateUserForm
 
-from django.views.generic.list import ListView
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
-# Create your views here.
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+        if request.method == "POST":
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for ' + user)
+                return redirect('login')
+        context = {'form': form}
+        return render(request, 'tasks/register.html', context)
 
+
+def login_page(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == "POST":
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, "Username OR password is incorrect")
+        context = {}
+        return render(request, 'tasks/login.html', context)
+
+
+def logout_page(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required(login_url='login')
 def home(request):
     tasks = Task.objects.all()
     context = {"tasks": tasks}
     return render(request, 'tasks/home.html', context)
 
 
+@login_required(login_url='login')
 def details(request):
     context = {}
     return render(request, 'tasks/details.html', context)
 
 
+@login_required(login_url='login')
 def add_user(request):
     form = TaskForm()
     if request.method == 'POST':
@@ -29,6 +71,7 @@ def add_user(request):
     return render(request, 'tasks/add_task.html', context)
 
 
+@login_required(login_url='login')
 def delete_task(request, pk):
     task = Task.objects.get(id=pk)
     if request.method == 'POST':
@@ -38,6 +81,7 @@ def delete_task(request, pk):
     return render(request, 'tasks/delete_task.html', context)
 
 
+@login_required(login_url='login')
 def edit_task(request, pk):
     task = Task.objects.get(id=pk)
     form = TaskForm(instance=task)
